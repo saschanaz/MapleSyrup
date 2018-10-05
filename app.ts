590fc9328901ef39b6d6c9da8792d455de994e0d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-05 02:12:13 +0000

    Deploy saschanaz/MapleSyrup to github.com/saschanaz/MapleSyrup.git:gh-pages

---
+++ app.ts
@@ -0,0 +1,37 @@
+"use strict";
+declare namespace dialogPolyfill {
+    function registerDialog(dialog: HTMLDialogElement): void;
+}
+
+declare var text: HTMLTextAreaElement;
+declare var result: HTMLTextAreaElement;
+declare var dialog: HTMLDialogElement;
+declare var agreeButton: HTMLInputElement;
+
+function syrupifySingleLine() {
+    try {
+        const resultText = MapleSyrup.convert(text.value.replace(/\s/g, ""));
+        result.textContent = resultText;
+    } catch (err) {
+        alert(`오류가 발생했습니다: ${err}`);
+    }
+}
+
+function syrupifyMultiLine() {
+    try {
+    const resultTexts = MapleSyrup.convertAsArray(text.value.replace(/\s/g, ""));
+    result.textContent = resultTexts.join("\r\n\r\n");
+    } catch (err) {
+        alert(`오류가 발생했습니다: ${err}`);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    agreeButton.addEventListener("click", () => {
+        dialog.close();
+    });
+    dialogPolyfill.registerDialog(dialog);
+    dialog.showModal();
+    dialog.addEventListener("cancel", ev => ev.preventDefault());
+    await (dialog.scrollTop = 0);
+});
