dd57b128187e2c0d2c1aa24ee598d0a501fd92d6
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-10-25 17:29:43 +0900

    remove lib from git [ci skip]

--- lib/maplesyrup.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function convert(mml: string | string[]): string;
-export declare function convertAsArray(mml: string | string[]): string[];
-interface Token {
-    type: string;
-    value: number;
-}
-export declare function parseChannel(mmlChannel: string): Token[];
-export {};
-export as namespace MapleSyrup;
